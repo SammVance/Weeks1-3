97f2e58ac579152eabfe7012b9c1c785e3dd22e4
SAM-DESKTOP\Sam <sam.mvance21@gmail.com>
2025-01-13 12:59:34 -0500

    Added Public Variable & Time

--- Assets/Scripts/FirstScript.cs
+++ Assets/Scripts/FirstScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    float speed = 0.01f;
+    public float speed = 0.01f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,15 +16,22 @@ public class FirstScript : MonoBehaviour
     void Update()
     {
         Vector2 pos = transform.position;
-        pos.x += speed;
+        pos.x += speed * Time.deltaTime;
 
         Vector2 screenPos = Camera.main.WorldToScreenPoint(pos);
 
-        if(screenPos.x < 0 || screenPos.x > Screen.width)
+        if(screenPos.x < 0)
         {
+            Vector3 fixedPos = new Vector3(0, 0, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
             speed = speed * -1;
         }
-
+        if (screenPos.x > Screen.width)
+        {
+            Vector3 fixedPos = new Vector3(Screen.width, 0, 0);
+            pos.x = Camera.main.ScreenToWorldPoint(fixedPos).x;
+            speed = speed * -1;
+        }
         transform.position = pos;
     }
 }
